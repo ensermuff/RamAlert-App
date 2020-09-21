ecaab56fdb1809f1378d7182330de70dbdde589b
allisondram <martinan4@vcu.edu>
2020-09-21 13:03:55 -0400

    implementation for dependencies needed for build.gradle file

--- app/src/main/java/com/vcu/teamnapp/MainActivity.java
+++ app/src/main/java/com/vcu/teamnapp/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+//Main Activity class
 public class MainActivity extends AppCompatActivity {
 
     @Override

788bf97596079b15c38cbabe0b1c12afb70f5f58
Rohit Karnati <karnatim@vcu.edu>
2020-09-25 17:12:52 -0400

    Reading SMS

--- app/src/main/java/com/vcu/teamnapp/MainActivity.java
+++ app/src/main/java/com/vcu/teamnapp/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         cursor.moveToFirst();
         myTextView.setText(cursor.getString(12));
 
+        //Create sms and read sms
     }
 
 

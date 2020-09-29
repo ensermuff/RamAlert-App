e31e190d6e85269f7d0cd6b589a4d00a5fd665f1
muzykafs <muzykafs123@gmail.com>
2020-09-29 17:31:48 -0400

    ft: text message retreival

---
+++ app/src/main/java/com/vcu/teamnapp/SMSListener.java
@@ -0,0 +1,26 @@
+package com.vcu.teamnapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+
+public class SMSListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        getMessage(context, intent);
+    }
+    public String getMessage(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                if (smsMessage.getOriginatingAddress().equals("795-16")) {
+                    String messageBody = smsMessage.getMessageBody();
+                    return messageBody;
+                }
+            }
+        }
+        return "";
+    }
+}
+
---
+++ app/src/test/java/com/vcu/teamnapp/MessageRetrievalTest.java
@@ -0,0 +1,17 @@
+package com.vcu.teamnapp;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import android.telephony.SmsMessage;
+
+
+public class MessageRetrievalTest {
+
+    public void messageRetriever(){
+        SMSListener testListener = new SMSListener();
+        assertThat();
+    }
+
+    private void assertThat() {
+
+    }
+}

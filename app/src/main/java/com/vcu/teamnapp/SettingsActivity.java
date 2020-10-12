1c29974976dde69338bc20bad6e599f8e09e3c2a
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2020-10-12 17:29:45 -0400

    Moved the code from Preference class to the settingsFragment class which is inside SettingsActivity.java

--- app/src/main/java/com/vcu/teamnapp/SettingsActivity.java
+++ app/src/main/java/com/vcu/teamnapp/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.vcu.teamnapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,7 +15,7 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
+                .replace(R.id.action_settings, new SettingsFragment())
                 .commit();
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -26,10 +28,27 @@ public class SettingsActivity extends AppCompatActivity {
         return true;
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements PreferenceManager.OnPreferenceTreeClickListener  {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            // The xml resource that holds the preferences
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+        @Override
+        public boolean onPreferenceTreeClick(androidx.preference.Preference preference) {
+            String key = preference.getKey();
+
+            if (key != null && key.equals("location")){
+                Intent myIntent = new Intent(SettingsFragment.this.getActivity(), MainActivity.class);
+                startActivity(myIntent);
+                return true;
+            } else if (key != null && key.equals("notifications")){
+                Intent myIntent2 = new Intent(SettingsFragment.this.getActivity(), EmergencyVCUNumbersActivity.class);
+                startActivity(myIntent2);
+                return true;
+            }
+
+            return false;
         }
     }
 }

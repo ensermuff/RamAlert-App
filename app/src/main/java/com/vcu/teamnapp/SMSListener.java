3f1830036096686e61cb1673739f0f20ab19a57e
muzykafs <muzykafs123@gmail.com>
2020-10-16 16:04:30 -0400

    ft: SMSListener accuracy fixes

--- app/src/main/java/com/vcu/teamnapp/SMSListener.java
+++ app/src/main/java/com/vcu/teamnapp/SMSListener.java
@@ -3,7 +3,7 @@ package com.vcu.teamnapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 

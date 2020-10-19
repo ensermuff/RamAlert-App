3fb838017ef1a9beee04342c2f21bef91cc24470
Rohit Karnati <karnatim@vcu.edu>
2020-10-19 13:15:29 -0400

    Added Nav Drawer Rough

--- app/src/main/java/com/vcu/teamnapp/MainActivity.java
+++ app/src/main/java/com/vcu/teamnapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

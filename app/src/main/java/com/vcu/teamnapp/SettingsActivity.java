04b5be36301fb35f30b92b32af86eadb87a754b5
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2020-10-24 17:06:50 -0400

    Created a separate activity that has the seekBar and created the action for the seekBar

--- app/src/main/java/com/vcu/teamnapp/SettingsActivity.java
+++ app/src/main/java/com/vcu/teamnapp/SettingsActivity.java
@@ -1,8 +1,6 @@
 package com.vcu.teamnapp;
 
 import android.os.Bundle;
-import android.widget.SeekBar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -19,24 +17,6 @@ public class SettingsActivity extends AppCompatActivity {
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        SeekBar mySeekBar = findViewById(R.id.seekBar);
-//        mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            int progressChanged = 0;
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                progressChanged = progress;
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//                Toast.makeText(SettingsActivity.this, "Distance in miles is: " + progressChanged, Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 
     @Override
@@ -51,21 +31,5 @@ public class SettingsActivity extends AppCompatActivity {
             // The xml resource that holds the preferences
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
-
-//        @Override
-//        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//            // To get an instance of a preference, search for a preference using its key through PreferenceManager within onCreateView()
-//            SwitchPreferenceCompat mySwitchPreference = (SwitchPreferenceCompat) getPreferenceManager().findPreference("location");
-//            if (mySwitchPreference != null) {
-//                mySwitchPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-//                    @Override
-//                    public boolean onPreferenceClick(Preference preference) {
-//                        return true;
-//                    }
-//                });
-//            }
-//            return inflater.inflate(R.layout.settings_activity, container, false);
-//        }
-
     }
 }

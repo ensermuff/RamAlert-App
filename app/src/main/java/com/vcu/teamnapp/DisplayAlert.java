c6c15f9dfbe4798072bbfcda66381bb0b3dde36b
muzykafs <muzykafs123@gmail.com>
2020-10-30 18:06:49 -0400

    Multiple alert functionality

--- app/src/main/java/com/vcu/teamnapp/DisplayAlert.java
+++ app/src/main/java/com/vcu/teamnapp/DisplayAlert.java
@@ -1,61 +1,90 @@
-//package com.vcu.teamnapp;
-//
-//import android.os.AsyncTask;
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.FragmentActivity;
-//
-//import com.google.android.gms.maps.CameraUpdateFactory;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import com.google.android.gms.maps.SupportMapFragment;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
-//import com.vcu.geocoder.Geocoder;
-//
-//import java.util.concurrent.ExecutionException;
-//
-//public class DisplayAlert extends FragmentActivity implements OnMapReadyCallback {
-//
-//    private GoogleMap mMap;
-//
-//    private String[] coordinates = new String[2];
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_display_alert);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//    }
-//
-//    /**
-//     * Manipulates the map once available.
-//     * This callback is triggered when the map is ready to be used.
-//     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-//     * we just add a marker near Sydney, Australia.
-//     * If Google Play services is not installed on the device, the user will be prompted to install
-//     * it inside the SupportMapFragment. This method will only be triggered once the user has
-//     * installed Google Play services and returned to the app.
-//     */
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//        AsyncTask task = new Geocoder().execute("VCU ALERT Robbery CORE MP Campus --Broad/Harrison. Police on scene. Avoid area.");
-//        try {
-//            coordinates = (String[]) task.get();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        }
-//        double lat = Double.parseDouble(coordinates[0]);
-//        double lon = Double.parseDouble(coordinates[1]);
-//        // Add a marker in Sydney and move the camera
-//        LatLng alert = new LatLng(lat, lon);
-//        mMap.addMarker(new MarkerOptions().position(alert).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(alert));
-//    }
-//}
+package com.vcu.teamnapp;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.vcu.geocoder.Geocoder;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+public class DisplayAlert extends FragmentActivity implements OnMapReadyCallback {
+
+    public static DisplayAlert Instance;
+    public static GoogleMap mMap;
+    private String vcuAlert = "";
+    private String[] coordinates = new String[2];
+    public static HashMap<LatLng, Marker> markerList = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setInstance();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_alert);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if(vcuAlert != null)
+            mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        setmMap(googleMap);
+        if(!vcuAlert.equals("")){
+            Geocoder geocoder = new Geocoder();
+            AsyncTask task = geocoder.execute(vcuAlert);
+            try {
+                coordinates = (String[]) task.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            double lat = Double.parseDouble(coordinates[0]);
+            double lon = Double.parseDouble(coordinates[1]);
+            //place a marker on our map
+            LatLng alert = new LatLng(lat, lon);
+            if(markerList.containsKey(alert)){
+                markerList.get(alert).remove();
+                markerList.clear();
+            }
+            else{
+                Marker amarker = mMap.addMarker(new MarkerOptions().position(alert).title("Vcu alert"));
+                markerList.put(alert, amarker);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(alert));
+            }
+        }
+
+    }
+    public void setVcuAlert(String alert){
+        vcuAlert = alert;
+    }
+    public void setmMap(GoogleMap map){
+        if(map != null){
+            mMap = map;
+        }
+    }
+    public void placeAlert(){
+        if (mMap != null) onMapReady(mMap);
+    }
+    public void setInstance(){
+        Instance = this;
+    }
+    public void removeMarkerFromList(Marker marker){
+        markerList.remove(marker);
+    }
+    public boolean isEmpty(){
+        return markerList.isEmpty();
+    }
+}
+

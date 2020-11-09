2cd6b9c0809c5009423d40f8aa00e76100f7e7c6
Firaol Ensermu <Firaolfayeensermu@gmail.com>
2020-11-09 13:04:23 -0500

    Updated locations and deleted second fragment

--- app/src/main/java/com/vcu/teamnapp/SettingsActivity2.java
+++ app/src/main/java/com/vcu/teamnapp/SettingsActivity2.java
@@ -1,11 +1,14 @@
 package com.vcu.teamnapp;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -17,25 +20,45 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class SettingsActivity2 extends AppCompatActivity {
+
     private static TextView myTextView;
+    private static TextView myTextView2;
+    private static TextView myTextView3;
     private static Switch mySwitch;
     private static boolean location_ON;
     private static boolean location_OFF;
     private LocationManager locationManager;
     private LocationListener locationListener;
-
+    private static double latitude;
+    private static double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings2);
+
+        myTextView2 = (TextView) findViewById(R.id.textView3);
+        myTextView3 = (TextView) findViewById(R.id.textView4);
+
+        seekBar();
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        checkLocationServices(locationManager);
+        locationListener();
+    }
+
+    private void locationListener() {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
 
+                //called whenever location is updated
+                myTextView2.setText("Latitude: " + latitude);
+                myTextView3.setText("Longitude: " + longitude);
             }
 
             @Override
@@ -50,11 +73,44 @@ public class SettingsActivity2 extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(@NonNull String provider) {
+                //checks if gps is turned off
+                alertMessageNoGps(locationManager);
 
             }
         };
-        seekBar();
-        checkLocationServices(locationManager);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 1);
+                return;
+            }
+        }else{
+            updateLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateLocation();
+                } return;
+        }
+    }
+
+    private void updateLocation() {
+        try {
+            // updates the location for every 5 seconds (5000 ms) and
+            // only if the user moved at least 5 meters from the previous location
+            locationManager.requestLocationUpdates("gps", 5000, 5, locationListener);
+            locationListener();
+
+        }catch (SecurityException e){
+            Toast.makeText(SettingsActivity2.this, "Request to update location permission is rejected, please turn on location services!", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void seekBar() {
@@ -81,7 +137,7 @@ public class SettingsActivity2 extends AppCompatActivity {
             }
         });
     }
-    public void checkLocationServices(final LocationManager MYMANGER){
+    public void checkLocationServices(final LocationManager locationManager){
         //check whether the phone's settings enabled location services for the app
         mySwitch = (Switch) findViewById(R.id.switch1);
         //Save switch state in shared preferences
@@ -98,11 +154,11 @@ public class SettingsActivity2 extends AppCompatActivity {
                     editor.putBoolean("value", true);
                     editor.apply();
                     mySwitch.setChecked(true);
-                    if (!MYMANGER.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                        alertMessageNoGps();
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                        alertMessageNoGps(locationManager);
                     }else{
                         // if the gps is enabled and location switch is turned on
-                        getCurrentLocation();
+                        updateLocation();
                     }
                 }else if (location_OFF) {
                     //When the switch isn't checked
@@ -110,23 +166,16 @@ public class SettingsActivity2 extends AppCompatActivity {
                     editor.putBoolean("value", false);
                     editor.apply();
                     mySwitch.setChecked(false);
-                    if (!MYMANGER.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                         Toast.makeText(SettingsActivity2.this, "Enable location services on your phone's settings!", Toast.LENGTH_LONG).show();
                     } else {
                         Toast.makeText(SettingsActivity2.this, "GPS is already enabled, please toggle switch!", Toast.LENGTH_SHORT).show();
-                    }
-                    if (location_OFF) {
-                        if (!MYMANGER.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                            Toast.makeText(SettingsActivity2.this, "Enable location services on your phone's settings!", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(SettingsActivity2.this, "GPS is already enabled, please toggle switch!", Toast.LENGTH_SHORT).show();
-                        }
                     }
                 }
             }
         });
     }
-    public void alertMessageNoGps(){
+    public void alertMessageNoGps(final LocationManager locationManager){
         //Alert dialog pops up if the location switch is on for the app but the location services is off for the phone settings
         AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
         myAlert.setTitle("Location")
@@ -135,6 +184,12 @@ public class SettingsActivity2 extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                            mySwitch.setChecked(location_OFF);
+                        }
+                        else {
+                            mySwitch.setChecked(location_ON);
+                        }
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -145,10 +200,5 @@ public class SettingsActivity2 extends AppCompatActivity {
                     }
                 });
         myAlert.create().show();
-        getCurrentLocation();
-    }
-    public void getCurrentLocation(){
-        // getting location
-
     }
 }

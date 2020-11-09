b29408b8f7cfb324407a0823d852f06216d76e1d
allisondram <martinan4@vcu.edu>
2020-11-09 13:49:23 -0500

    Creating new alerts activity (textview successfully displays but crime chip group not properly adding)

---
+++ app/src/main/java/com/vcu/teamnapp/CrimeChipGroup.java
@@ -0,0 +1,14 @@
+package com.vcu.teamnapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrimeChipGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_chip_group);
+    }
+}

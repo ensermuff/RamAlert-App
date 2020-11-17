e0ef921e4ac3754f8115eee81adaba508db264ee
allisondram <martinan4@vcu.edu>
2020-11-17 15:42:29 -0500

    Chip xml added to alerts page

--- app/src/main/java/com/vcu/teamnapp/ChipGroup.java
+++ app/src/main/java/com/vcu/teamnapp/ChipGroup.java
@@ -2,7 +2,9 @@ package com.vcu.teamnapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChipGroup extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class ChipGroup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chip_group);
     }
+//
+//    public void goBackToMain(View view) {
+//        Intent intent = new Intent(ChipGroup.this,MainActivity.class);
+//        startActivity(intent);
+//    }
 }

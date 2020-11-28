eef60431c4f5e69e71d69d52e1df23ee1001b5a8
allisondram <martinan4@vcu.edu>
2020-11-28 18:31:43 -0500

    Adding chip group to merged master

---
+++ app/src/main/java/com/vcu/RamAlerts/ChipGroup.java
@@ -0,0 +1,14 @@
+package com.vcu.RamAlerts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChipGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chip_group);
+    }
+}
